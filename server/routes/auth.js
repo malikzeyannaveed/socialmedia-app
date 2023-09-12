c5a8be4e6bd0d1aeb97317bccb53e347344ea99a
malikzeyannaveed <144803758+malikzeyannaveed@users.noreply.github.com>
2023-09-12 17:47:05 +0500

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt')
-
+const JWT_SECRET = 'thisisasecretkey'
 
 //Done
 router.post("/login",async (req, res) => {
@@ -17,7 +17,7 @@ router.post("/login",async (req, res) => {
       if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
   const userr = JSON.parse(JSON.stringify(user));
   delete userr.password
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id }, JWT_SECRET);
       res.status(200).json({ token, userr });
     } catch (err) {
       res.status(500).json({ error: err.message });
